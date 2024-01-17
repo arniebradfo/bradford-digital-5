7a60a0a8c59dfb75017f81acc0ba7bcfe4a85595
James Bradford <j.bradford@pnnl.gov>
2024-01-16 17:01:14 -0800

    add image to page

--- app/page.tsx
+++ app/page.tsx
@@ -3,25 +3,26 @@ import { Button } from "./components/Button";
 
 export default function Home() {
   return (
-    <main>
-      <h1 style={{
-        display: 'flex',
-        justifyContent: 'center',
-        gap: 24,
-        alignItems: 'center',
-        padding: 64
-      }}>
+    <main style={{ margin: "0 auto", maxWidth: 1000 }}>
+      <h1
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          gap: 24,
+          alignItems: "center",
+          padding: 64,
+        }}
+      >
         <Image
           src="/logo/hexagon.inline.svg"
           alt="Vercel Logo"
-          className="dark:invert"
-          width={16*3}
-          height={16*3}
+          width={16 * 3}
+          height={16 * 3}
           priority
         />{" "}
         <span>bradford.digital</span>
-        <Button>BUTTON!</Button>
       </h1>
+      <img src="/lfs-media/AvyMap_Mockup-Desktop.png" alt="AvyMap app" />
     </main>
   );
 }
