b96f1a2d303ad32b6c01787e19ad4e348eb47c55
James Bradford <j.bradford@pnnl.gov>
2024-02-09 10:21:54 -0800

    Revert "tried more complex tag props"
    This reverts commit 46beac4a95084df5ec8d40495e9190764735adc1.

--- app/components/Text.tsx
+++ app/components/Text.tsx
@@ -1,14 +1,11 @@
-import React, { ComponentProps, HTMLAttributes } from "react";
+import React, { HTMLAttributes } from "react";
 import style from "./Text.module.css";
 
-// THIS?: https://stackoverflow.com/a/54049872/5648839 //
-type TagProps<Tag> = Tag extends keyof JSX.IntrinsicElements ? JSX.IntrinsicElements[Tag] : never
-
-export type TextProps<TagName extends keyof JSX.IntrinsicElements> = TagProps<TagName> & {
+export type TextProps = HTMLAttributes<HTMLOrSVGElement> & {
   /** type of tag to use */
-  tagName?: TagName;
+  tagName?: keyof JSX.IntrinsicElements;
 
-  // COLOR //
+  // COLOR
   /** #1 font color  */
   heading?: boolean;
   /** #2 font color - default  */
@@ -18,7 +15,7 @@ export type TextProps<TagName extends keyof JSX.IntrinsicElements> = TagProps<Ta
   /** #4 font color  */
   disabled?: boolean;
 
-  // SIZE //
+  // SIZE
   /** large font-size  */
   large?: boolean;
   /** medium font-size  */
@@ -28,7 +25,7 @@ export type TextProps<TagName extends keyof JSX.IntrinsicElements> = TagProps<Ta
   /** header font-sizes */
   h?: number;
 
-  // FONT STYLE //
+  // FONT STYLE
   /** bold font-weight  */
   bold?: boolean;
   /** normal font-weight - default (ie not bold)  */
@@ -36,7 +33,7 @@ export type TextProps<TagName extends keyof JSX.IntrinsicElements> = TagProps<Ta
   /** italic font-style  */
   italic?: boolean;
 
-  // FAMILY / STYLE //
+  // FAMILY / Style
   /** sans-serif font-family - default */
   sans?: boolean;
   /** monospace font-family (ie code) */
@@ -49,8 +46,8 @@ export type TextProps<TagName extends keyof JSX.IntrinsicElements> = TagProps<Ta
 };
 
 /** A utility text component for declarative styling */
-export const Txt = <TagName extends keyof JSX.IntrinsicElements>({
-  tagName: RootTag = 'span',
+export const Txt: React.FC<TextProps> = ({
+  tagName,
 
   heading,
   body,
@@ -67,14 +64,100 @@ export const Txt = <TagName extends keyof JSX.IntrinsicElements>({
   italic,
 
   sans,
-  monospace,
+  monospace: mono,
   meta,
 
   ellipsize,
-
   className,
   ...props
-}: TextProps<TagName>) => {
+}) => {
+  /* const txtCss = useMemo(
+    () => [
+      {
+        color: body //
+          ? Tkn.TextBody
+          : heading
+          ? Tkn.TextHeading
+          : muted
+          ? Tkn.TextMuted
+          : disabled
+          ? Tkn.TextDisabled
+          : undefined,
+      },
+      {
+        fontSize: medium //
+          ? Tkn.FontSizeMedium
+          : large
+          ? Tkn.FontSizeLarge
+          : small
+          ? Tkn.FontSizeSmall
+          : undefined,
+      },
+      meta && utilityStyles.textMeta, // needs to come before fontWeight
+      {
+        fontWeight: normal //
+          ? (Tkn.FontWeightNormal as any)
+          : bold
+          ? (Tkn.FontWeightBold as any)
+          : undefined,
+      },
+      {
+        fontStyle: normal //
+          ? "normal"
+          : italic
+          ? "italic"
+          : undefined,
+      },
+      {
+        fontFamily: sans
+          ? Tkn.FontFamilySans
+          : mono
+          ? Tkn.FontFamilyMonospace
+          : serif
+          ? Tkn.FontFamilySerif
+          : undefined,
+      },
+      {
+        display: block ? "block" : undefined,
+      },
+      running && utilityStyles.textRunning,
+      ellipsize && utilityStyles.textEllipsis,
+      alignTxtIcons,
+    ],
+    [
+      block,
+      bold,
+      disabled,
+      ellipsize,
+      heading,
+      italic,
+      large,
+      medium,
+      meta,
+      mono,
+      muted,
+      normal,
+      body,
+      running,
+      sans,
+      serif,
+      small,
+    ]
+  );
+
+  const _className = useMemo(() => {
+    const classNames = [];
+    if (skeleton) classNames.push(Classes.SKELETON);
+    if (className) classNames.push(className);
+    return classNames.length > 0 ? classNames.join(" ") : undefined;
+  }, [className, skeleton]);
+
+  const RootTag = useMemo(() => {
+    return tagName ?? (running ? "p" : "span");
+  }, [running, tagName]);
+ */
+  
+  const RootTag = tagName ?? "span";
 
   return <RootTag className={style.Text} {...props} />;
 };
