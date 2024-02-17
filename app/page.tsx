7cae160dc1fb218a89fff418e6c0f399607749dc
James Bradford <j.bradford@pnnl.gov>
2024-02-17 11:53:46 -0800

    use Text Components

--- app/page.tsx
+++ app/page.tsx
@@ -2,20 +2,36 @@ import Image from "next/image";
 import AvyMapMockupDesktop from "../public/lfs-media/AvyMap_Mockup-Desktop.png";
 import Link from "next/link";
 import { externalLinkAttributes } from "./utils/link";
+import { Txt } from "./components/Text";
+import { Spacer } from "./components/Spacer";
 
 export default function Home() {
   return (
     <div>
-      <header>
+      <header style={{ padding: 48, position: "relative" }}>
         <Image
           src="/logo/small.svg"
           alt="jb logo"
           width={16 * 3}
           height={16 * 3}
           priority
+          style={{ marginBottom: 48 }}
         />
-        <h1>James Bradford / UX Engineer</h1>
-        <p>
+
+        <Txt tag="h1" size={2} fg={1} uppercase style={{ marginBottom: 24 }}>
+          <Txt>James Bradford</Txt>
+          <Spacer normal>/</Spacer>
+          <Txt fg={3}>UX Engineer</Txt>
+        </Txt>
+
+        <Txt tag="p" style={{ maxWidth: 640 }}>
+          <Txt bold>
+            User Research
+            <Spacer>/</Spacer>
+            UX Design
+            <Spacer>/</Spacer> UI Development
+          </Txt>
+          <Spacer>-</Spacer>
           Currently working in the{" "}
           <Link
             href={"https://www.pnnl.gov/visual-analytics"}
@@ -26,11 +42,13 @@ export default function Home() {
           at{" "}
           <Link href={"https://www.pnnl.gov/"} {...externalLinkAttributes}>
             Pacific Northwest National Laboratory
-          </Link>{" "}
-          making web apps for cybersecurity, geospatial, machine learning, and
-          data visualization.
-        </p>
-        <nav>
+          </Link>
+          {"'s "}
+          Seattle Office making web apps for cybersecurity, geospatial analysis,
+          machine learning, and data visualization.
+        </Txt>
+
+        <nav style={{ position: "absolute", right: 0, top: 0, margin: 48 }}>
           {[
             {
               children: "LinkedIn",
@@ -57,7 +75,7 @@ export default function Home() {
             },
           ].map((linkProps, i) => (
             <>
-              {i > 0 && <span>/</span>}
+              {i > 0 && <Spacer>/</Spacer>}
               <Link {...linkProps} />
             </>
           ))}
