c056f1f381f6fd7a1e8229b0d83595ebf5aea8a8
James Bradford <j.bradford@pnnl.gov>
2024-02-29 09:13:50 -0800

    add link content

---
+++ app/views/linkContent.tsx
@@ -0,0 +1,112 @@
+import ImgAdobeXdRepeatText from "../../public/lfs-media/Project-Icons/Project-Icon-Adobe-Xd-Repeat-Text.svg";
+import ImgAvyMap from "../../public/lfs-media/Project-Icons/Project-Icon-AvyMap.svg";
+import ImgBlueprintFigma from "../../public/lfs-media/Project-Icons/Project-Icon-Blueprint-Figma-Components.svg";
+import ImgBlueprintStyler from "../../public/lfs-media/Project-Icons/Project-Icon-Blueprint-Styler.svg";
+import ImgFigmaAutoConstraints from "../../public/lfs-media/Project-Icons/Project-Icon-Figma-Auto-Constraints.svg";
+import ImgFigmaRandomLayers from "../../public/lfs-media/Project-Icons/Project-Icon-Figma-Random-Layers.svg";
+import ImgFigmaRepeatAsList from "../../public/lfs-media/Project-Icons/Project-Icon-Figma-Repeat-As-List.svg";
+import ImgFigmaStyleQuickActions from "../../public/lfs-media/Project-Icons/Project-Icon-Figma-Style-Quick-Actions.png";
+import ImgHydropowerELibrary from "../../public/lfs-media/Project-Icons/Project-Icon-Hydropower-eLibrary.svg";
+import ImgIrrigationViz from "../../public/lfs-media/Project-Icons/Project-Icon-IrrigationViz.svg";
+import ImgRedEye from "../../public/lfs-media/Project-Icons/Project-Icon-RedEye.svg";
+import ImgWordpress from "../../public/lfs-media/Project-Icons/Project-Icon-Wordpress.svg";
+
+export const linkContent = {
+  pluginWordpress: {
+    label: "Wordpress",
+    header: "HTML Syntax Highlighter",
+    description: "WP admin's missing IDE",
+    imgSrc: ImgWordpress,
+    imgAlt: "",
+    href: "https://wordpress.org/plugins/html-editor-syntax-highlighter/",
+  },
+  pluginFigmaAutoConstraints: {
+    label: "Figma",
+    header: "Auto Constraints",
+    description: "Automagically apply layout constraints",
+    imgSrc: ImgFigmaAutoConstraints,
+    imgAlt: "",
+    href: "https://www.figma.com/community/plugin/1155285435916019216/auto-constraints",
+  },
+  pluginFigmaRepeatList: {
+    label: "Figma",
+    header: "Repeat As List",
+    description: "Quickly create a list linked to a master component",
+    imgSrc: ImgFigmaRepeatAsList,
+    imgAlt: "",
+    href: "https://www.figma.com/community/plugin/1201323323265727466/repeat-as-list",
+  },
+  pluginFigmaStyleQuickActions: {
+    label: "Figma",
+    header: "Style Quick Actions",
+    description: "Apply Styles and Variables from the QA toolbar",
+    imgSrc: ImgFigmaStyleQuickActions,
+    imgAlt: "",
+    href: "https://www.figma.com/community/plugin/1227005108437065902/style-quick-actions",
+  },
+  pluginFigmaSelectRandom: {
+    label: "Figma",
+    header: "Select Random Layers",
+    description: "Select a random subset of the current selection",
+    imgSrc: ImgFigmaRandomLayers,
+    imgAlt: "",
+    href: "https://www.figma.com/community/plugin/1227013202162788533/select-random-layers",
+  },
+  pluginAdobeXdRepeatText: {
+    label: "Adobe Xd",
+    header: "Repeat Grid Text Editor",
+    description: "Rapidly edit several text objects as a single input",
+    imgSrc: ImgAdobeXdRepeatText,
+    imgAlt: "",
+    href: "https://github.com/arniebradfo/xd-plugin-repeat-grid-text-editor",
+  },
+  blueprintStyler: {
+    label: "Design System Extension",
+    header: "Blueprint Styler",
+    description: "Add custom themes to Palantir's Blueprint component library",
+    imgSrc: ImgBlueprintStyler,
+    imgAlt: "",
+    href: "https://pnnl.github.io/blueprint-styler/",
+  },
+  blueprintComponents: {
+    label: "Design System Extension",
+    header: "Blueprint Figma Library",
+    description:
+      "A (mostly) 1:1 match to Palantir's Blueprint component library",
+    imgSrc: ImgBlueprintFigma,
+    imgAlt: "",
+    href: "https://www.figma.com/@pnnl",
+  },
+  projectRedeye: {
+    label: "PNNL / CISA",
+    header: "RedEye",
+    description: "RedTeam cybersecurity assessment visualization ",
+    imgSrc: ImgRedEye,
+    imgAlt: "",
+    href: "https://github.com/cisagov/RedEye",
+  },
+  projectHydropowerELibrary: {
+    label: "PNNL / WPTO",
+    header: "Hydropower eLibrary",
+    description: "A repository of FERC licensed hydropower projects",
+    imgSrc: ImgHydropowerELibrary,
+    imgAlt: "",
+    href: "https://hydropowerelibrary.pnnl.gov/",
+  },
+  projectIrrigationViz: {
+    label: "PNNL / WPTO",
+    header: "IrrigationViz",
+    description: "Visualize and compare upgrades to an irrigation district",
+    imgSrc: ImgIrrigationViz,
+    imgAlt: "",
+    href: "https://irrigationviz.pnnl.gov/",
+  },
+  projectAvyMap: {
+    label: "Personal Project",
+    header: "AvyMap",
+    description: "Visualize avalanche danger of any slope",
+    imgSrc: ImgAvyMap,
+    imgAlt: "",
+    href: "https://www.avymap.com/#7/48/-121",
+  },
+};
