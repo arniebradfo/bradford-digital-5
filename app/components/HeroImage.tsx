5926f532bed345f80518ede7ae747852b8234754
James Bradford <j.bradford@pnnl.gov>
2024-03-24 18:24:29 -0700

    HeroImage is looking pretty good

--- app/components/HeroImage.tsx
+++ app/components/HeroImage.tsx
@@ -12,7 +12,6 @@ import { useMagneticParallax } from "../utils/useMagneticParallax";
 import { CSSProperties, useRef } from "react";
 import {
   KeyframeOptions,
-  MotionValue,
   animate,
   motion,
   useMotionTemplate,
@@ -51,82 +50,80 @@ export const HeroImage: React.FC<React.ComponentProps<"div">> = ({
 
   const t1 = useMagneticParallaxLayers({
     ...motionValues,
-    scaleMore: scaleMore * 1,
+    scaleMore: scaleUp + scaleMore * 1,
     translateMore: translateMore * 1,
   });
   const t2 = useMagneticParallaxLayers({
     ...motionValues,
-    scaleMore: scaleMore * 2,
+    scaleMore: scaleUp + scaleMore * 2,
     translateMore: translateMore * 2,
   });
   const t3 = useMagneticParallaxLayers({
     ...motionValues,
-    scaleMore: scaleMore * 3,
+    scaleMore: scaleUp + scaleMore * 3,
     translateMore: translateMore * 3,
   });
   const t4 = useMagneticParallaxLayers({
     ...motionValues,
-    scaleMore: scaleMore * 4,
+    scaleMore: scaleUp + scaleMore * 4,
     translateMore: translateMore * 4,
   });
 
   const transforms = [t1, t2, t3, t4];
 
+  const customVar = { "--duration": duration + "s" } as CSSProperties;
+
   return (
-    <div style={{ perspective: "1000px" }}>
+    <motion.div
+      className={style.ImageLayers}
+      ref={elementRef}
+      onHoverStart={(mouseEvent) => {
+        startMagneticParallax({ mouseEvent });
+        animate(scale0, 1, { duration, ease: "easeInOut" });
+      }}
+      onHoverEnd={(mouseEvent) => {
+        endMagneticParallax({ mouseEvent });
+        animate(scale0, 0, { duration, ease: "easeInOut" });
+      }}
+      onMouseMove={(mouseEvent) => {
+        updateMagneticParallax({ mouseEvent });
+      }}
+      style={customVar}
+    >
       <motion.div
-        className={style.ImageLayers}
-        ref={elementRef}
-        onHoverStart={(mouseEvent) => {
-          startMagneticParallax({ mouseEvent });
-          animate(scale0, 1, { duration, ease: "easeInOut" });
-        }}
-        onHoverEnd={(mouseEvent) => {
-          endMagneticParallax({ mouseEvent });
-          animate(scale0, 0, { duration, ease: "easeInOut" });
-        }}
-        onMouseMove={(mouseEvent) => {
-          updateMagneticParallax({ mouseEvent });
-        }}
+        className={jCN([style.ImageLayer, style.ImageBackground])}
         style={{
-          // transformStyle: "preserve-3d",
-          // transform,
-          perspective: "1000px"
+          backgroundColor: "#121212",
+          transform: t1,
         }}
-      >
-        {/* add div background */}
+      />
+      <MotionImage
+        src={Layer4}
+        className={style.ImageLayer}
+        alt="a test image"
+        style={{
+          position: "relative",
+          transform: t1,
+        }}
+      />
+      {[Layer3, Layer2, Layer1].map((layer, i) => (
         <MotionImage
-          src={Layer4}
-          className={style.ImageBase}
+          key={i}
+          src={layer}
           alt="a test image"
+          className={style.ImageLayer}
           style={{
-            // x: x1,
-            // y: y1,
-            transform: t1,
-            transformStyle: "preserve-3d",
-            transformOrigin: "50% 50%",
+            transform: transforms[i + 1],
           }}
         />
-        {[Layer3, Layer2, Layer1].map((layer, i) => (
-          <MotionImage
-            key={i}
-            src={layer}
-            alt="a test image"
-            className={style.ImageLayer}
-            style={{
-              transform: transforms[i + 1],
-              transformStyle: "preserve-3d",
-              transformOrigin: "50% 50%",
-            }}
-          />
-        ))}
-      </motion.div>
-    </div>
+      ))}
+    </motion.div>
   );
 };
 
 const MotionImage = motion(Image);
 
+const scaleUp = 0.015;
 const scaleMore = 0.005;
 const translateMore = 2;
 const duration = 0.15;
