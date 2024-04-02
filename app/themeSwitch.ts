576bd982bb97fa146f2e8c799f3dc8ffe81c7db7
James Bradford <j.bradford@pnnl.gov>
2024-04-02 16:30:20 -0700

    add theme switch code

--- app/themeSwitch.ts
+++ app/themeSwitch.ts
@@ -1,3 +1,20 @@
 export const initializeTheme = /*js*/ `
-  console.log('hello');
+
+if (document != null && window != null && window.matchMedia) {
+  function updateThemeBasedOnPreference(e) {
+    if (e.matches) {
+      // Dark mode
+      document.documentElement.classList.add('dark-mode')
+      document.documentElement.classList.remove('light-mode')
+    } else {
+      // Light mode
+      document.documentElement.classList.remove('dark-mode')
+      document.documentElement.classList.add('light-mode')
+    }
+  }
+  const prefersDarkScheme = window.matchMedia('(prefers-color-scheme: dark)');
+  updateThemeBasedOnPreference(prefersDarkScheme);
+  prefersDarkScheme.addEventListener('change', updateThemeBasedOnPreference);
+}
+
 `;
