239b391bb2cec29a5fad04b445236ebe9b4edcb6
James Bradford <j.bradford@pnnl.gov>
2024-04-02 17:28:57 -0700

    suppressHydrationWarning on html element

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,13 +25,20 @@ export const metadata: Metadata = {
     "James Bradford is UX Engineer (User Research, UX Design, and UI Development) currently working in the Human Centered Computing Group at Pacific Northwest National Laboratory's Seattle Office making web apps for cybersecurity, geospatial analysis, machine learning, and data visualization.",
 };
 
+console.log(PlexSans);
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={jCN([PlexSans.variable, PlexMono.variable])} data-theme="light">
+    <html
+      lang="en"
+      className={jCN([PlexSans.variable, PlexMono.variable])}
+      data-theme="light"
+      suppressHydrationWarning
+    >
       <body>
         <Script id="initializeTheme" strategy="beforeInteractive">
           {initializeTheme}
