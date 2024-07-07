2146939bcae0011609822d7790804784954ec492
James Bradford <j.bradford@pnnl.gov>
2024-07-07 14:04:13 -0700

    add dynamic height and width to useMDXComponents Img

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,3 +1,5 @@
+import path from "path";
+import getImageSize from "image-size";
 import type { MDXComponents } from "mdx/types";
 import Image, { ImageProps } from "next/image";
 import { B, H1, H2, H3, H4, H5, H6, I, Txt } from "./app/components/Text";
@@ -13,17 +15,42 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
     h6: (p) => <H6 {...p} />,
     strong: (p) => <B {...p} />,
     em: (p) => <I {...p} />,
-    img: ({ src, ...props }) => (
-      <Image
-        sizes="100vw"
-        width={100}
-        height={100}
-        // fill
-        style={{ width: "100%", height: "auto" }}
-        src={src?.replace("public/", "/") || ''}
-        {...(props as Omit<ImageProps, 'src'>)}
-      />
-    ),
+    img: Img,
     ...components,
   };
 }
+
+/** https://shaneafsar.com/blog/markdown-mdx-next-images/ */
+const Img = ({ ...props }: React.DetailedHTMLProps<React.ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>) => {
+  // Copy props into new object since it's locked
+  const newProps = { ...props };
+
+  let newSrc = props.src || '' // TODO: error image?
+
+  const isLocalImage = !newSrc.startsWith("http");
+
+  // If no dimensions are defined, let's find it!
+  if (!props.width && !props.height && isLocalImage) {
+    // Extract the file name and path. You may need to adjust this for your app.
+    const fileName = newSrc.replace("/images", ""); // e.g. file.png, or /subdir/file.png
+    const filePath = path.join(process.cwd(), fileName);
+
+    const dimensions = getImageSize(filePath);
+
+    newProps.width = dimensions.width;
+    newProps.height = dimensions.height;
+    
+  }
+
+  /** 
+   * If you have a different basePath for your application (e.g. site.xyz/docs/),
+   * you might want to hardcode that here so that you don't need to write it for
+   * every image.
+   */
+  if (/* !newSrc.startsWith(basePath) &&  */isLocalImage) {
+    newProps.src = `${basePath}${newSrc?.replace("public/", "/")}`;
+  }
+  return <Image {...(newProps as ImageProps)} />;
+}
+
+const basePath = ''
