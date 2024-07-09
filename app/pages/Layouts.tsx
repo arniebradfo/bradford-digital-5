fe546c29b9a3891b373f8bd0962aaded13095f3f
James Bradford <j.bradford@pnnl.gov>
2024-07-08 18:32:37 -0700

    Layout Columns

--- app/pages/Layouts.tsx
+++ app/pages/Layouts.tsx
@@ -5,36 +5,31 @@ type HtmlProps<T extends keyof JSX.IntrinsicElements = "div"> =
   React.ComponentProps<T>;
 
 const ColumnFull: React.FC<HtmlProps> = ({ className, ...props }) => (
-  <div className={cx(className, style.ColumnFull)} {...props} />
+  <div
+    className={cx(className, style.ColumnFull, style.ColumnContainer)}
+    {...props}
+  />
 );
 
 const ColumnMax: React.FC<HtmlProps> = ({ className, children, ...props }) => (
-  <div className={cx(className, style.ColumnMax)} {...props}>
+  <div
+    className={cx(className, style.ColumnMax, style.ColumnContainer)}
+    {...props}
+  >
     {children}
   </div>
 );
 
 const Columns: React.FC<
   HtmlProps & { wrap?: boolean; count?: 1 | 2 | 3 | 4 | 5 | 6 }
-> = ({ className, children, wrap = true, count = 3, ...props }) => {
-  // console.log({ children });
-
-  // const childCount = Array.isArray(children) ? children.length : 1;
-
-  return (
-    <div
-      className={cx(
-        className,
-        style.ColumnMax,
-        style.Columns,
-        styleColumnCount[count - 1]
-      )}
-      {...props}
-    >
-      {children}
-    </div>
-  );
-};
+> = ({ className, children, wrap = true, count = 1, ...props }) => (
+  <div
+    className={cx(className, style.Columns, styleColumnCount[count - 1])}
+    {...props}
+  >
+    {children}
+  </div>
+);
 
 const styleColumnCount = [
   style.Columns1,
