fd335ebf793d86e126d035e172b7006eff8caa82
James Bradford <j.bradford@pnnl.gov>
2024-07-11 11:24:15 -0700

    add openGraph metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,7 @@ import { AsciiArt } from "./components/AsciiArt";
 import { initializeTheme } from "./utils/themeSwitch";
 import Script from "next/script";
 import { SetClientWidthCss } from "./utils/SetClientWidthCss";
+import ogImage from "../public/lfs-media/og_image.png";
 
 import "./reset.css";
 import "./globals.css";
@@ -12,7 +13,7 @@ import "./globals.css";
 const PlexSans = IBM_Plex_Sans({
   weight: ["400", "600"],
   subsets: ["latin"],
-  style: ['italic','normal'],
+  style: ["italic", "normal"],
   variable: "--plex-font-family-sans",
 });
 
@@ -26,6 +27,15 @@ export const metadata: Metadata = {
   title: "James Bradford / UX Engineer",
   description:
     "James Bradford is UX Engineer (User Research, UX Design, and UI Development) currently working in the Human Centered Computing Group at Pacific Northwest National Laboratory's Seattle Office making web apps for cybersecurity, geospatial analysis, machine learning, and data visualization.",
+  openGraph: {
+    images: {
+      url: ogImage.src,
+      alt: "Graphic with jb logo and text: James Bradford / UX Design / UI Development",
+      type: "image/png",
+      width: 1200,
+      height: 630,
+    },
+  },
 };
 
 export default function RootLayout({
@@ -36,7 +46,7 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={cx(PlexSans.variable, /* PlexMono.variable */)}
+      className={cx(PlexSans.variable /* PlexMono.variable */)}
       data-theme="light"
       suppressHydrationWarning
     >
