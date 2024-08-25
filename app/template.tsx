8d8dadc1f654c9071977acb69619b0a9a655277e
James Bradford <james@bradford.digital>
2024-08-25 16:22:59 -0400

    restore old template wrapper animations

--- app/template.tsx
+++ app/template.tsx
@@ -5,9 +5,9 @@ import { motion } from "framer-motion";
 export default function Template({ children }: { children: React.ReactNode }) {
   return (
     <motion.div
-      // initial={{ y: -40, opacity: 0 }}
-      // animate={{ y: 0, opacity: 1 }}
-      // transition={{ ease: [0.2, 0, 0, 0.75], duration: 1 }}
+      initial={{ y: -40, opacity: 0 }}
+      animate={{ y: 0, opacity: 1 }}
+      transition={{ ease: [0, 0, 0, 1], duration: 1 }}
     >
       {children}
     </motion.div>
